4dba73a585de46f0a4733c2a2b47aeb72148faf0
Rosario Polito <rosario.polito@gmx.de>
2019-02-18 14:14:51 +0100

    gefühle > fakten

---
+++ EANISBN/EANGenerating.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class EANGenerating
+    {
+        public static string Start(string land)
+        {
+            int[] EAN = new int[13];
+            int checkSum = 0, checkMod;
+            Random rndInt = new Random();
+
+            switch (land)
+            {
+                case "DE":
+                    EAN[0] = 4;
+                    EAN[1] = 0;
+                    break;
+
+                case "CH":
+                    EAN[0] = 7;
+                    EAN[1] = 6;
+                    break;
+
+                case "AU":
+                    EAN[0] = 9;
+                    EAN[1] = 0;
+                    break;
+
+                default:
+                    EAN[0] = 4;
+                    EAN[1] = 0;
+                    break;
+            }
+
+            for (var i = 3; i < EAN.Length - 2; i++)
+            {
+                var x = rndInt.Next(0, 10);
+                EAN[i] = x;
+            }
+
+            for (var i = 0; i < EAN.Length - 1; i++)
+            {
+                if (!(EAN[i] % 2 == 0))
+                {
+                    checkSum += EAN[i] * 3;
+                }
+                else
+                {
+                    checkSum += EAN[i];
+                }
+            }
+
+            checkMod = checkSum % 10;
+            EAN[12] = checkMod;
+
+            var EANString = string.Join("", EAN);
+
+            return EANString;
+        }
+    }
+}
---
+++ EANISBN/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.Banner();
+            UI.Menue();
+            int select = Int32.Parse(Console.ReadLine());
+            string ISBN;
+            string EAN;
+
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine("Gib eine ISBN an, die geprüft werden soll:");
+                    ISBN = Console.ReadLine();
+                    Check.CheckISBN(ISBN);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Eine EAN wird generiert.");
+                    EAN = EANGenerating.Start("DE");
+                    Console.WriteLine("EAN wird in die csv geschreiben.");
+                    CSVGeneration.Start("", EAN);
+                    Console.WriteLine("EAN würde in output.csv gespeichert.");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Eine EAN wird generiert.");
+                    ISBN = ISBNGenerating.Start();
+                    Console.WriteLine("EAN wird in die csv geschreiben.");
+                    CSVGeneration.Start("", ISBN);
+                    Console.WriteLine("EAN würde in output.csv gespeichert.");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Land auswählen");
+                    string land = Console.ReadLine();
+                    CSVGeneration.Start("", EANGenerating.Start(land));
+                    break;
+
+                case 5:
+                    Console.WriteLine("Die output.csv wird ausgelesen.");
+                    foreach (var entry in ReadCSV.Read())
+                    {
+                        Console.WriteLine(entry);
+                    }
+                    break;
+
+                default:
+                    Environment.Exit(0x0);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
